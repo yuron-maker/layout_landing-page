1080001d0cd13bc67fe4257ad765f0e680e3b7d6
yuron-maker <komanitskiy200320@gmail.com>
2024-11-08 18:04:51 +0200

    make build

---
+++ index.94029fd8.js
@@ -0,0 +1,2 @@
+window.addEventListener("hashchange",function(){"#menu"===window.location.hash?document.body.classList.add("page__body--with-menu"):document.body.classList.remove("page__body--with-menu")});var e=document.querySelector("#id-submit"),t=document.querySelector("#id-Form");e.addEventListener("click",function(e){e.preventDefault(),t.reset()});
+//# sourceMappingURL=index.94029fd8.js.map
